6125674764d657ed23dd22a8bb606aba760e1325
Prableen Singh Matharu <130837233+Prabhsingh0401@users.noreply.github.com>
2025-06-10 00:20:12 +0530

    Added Packages

--- frontend/src/components/Providers/index.js
+++ frontend/src/components/Providers/index.js
@@ -1,7 +1,5 @@
-// Export all providers from this directory
 import { CalmModeProvider } from './CalmModeContext';
 import { AudioDescriptionProvider } from '../AudioDescription/AudioDescriptionContext';
 import ReactProvider from './ReactProvider';
 
-// Export the providers
 export { ReactProvider, CalmModeProvider, AudioDescriptionProvider };
